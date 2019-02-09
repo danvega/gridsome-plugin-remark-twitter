fd41067956a79f15b2f2412f36e8d25f66d7b244
Dan Vega <danvega@gmail.com>
2019-02-09 11:23:27 -0500

    Refactoring the getEmbeddedTweet() function

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const visit = require('unist-util-visit');
 const fetch = require('node-fetch');
 
-const getEmbeddedTweet = async (url, opt) => {
-  const apiUrl = `https://publish.twitter.com/oembed?url=${
-    url
-  }&hide_thread=${
-    opt.hideThread !== false ? '1' : '0'
-  }&align=${
-    opt.align || ''
-  }&hide_media=${
-    opt.hideMedia ? '1' : '0'
-  }&theme=${
-    opt.theme || ''
-  }&link_color=${
-    opt.linkColor || ''
-  }&widget_type=${
-    opt.widgetType || ''
-  }&omit_script=true&dnt=true&limit=20&chrome=nofooter`
-
-  const response = await fetch(apiUrl);
-  return await response.json();
-};
-
 const momentRegexp = /https:\/\/twitter.com\/i\/moments\/[0-9]+/i;
 const tweetRegexp = /https:\/\/twitter\.com\/[A-Za-z0-9-_]*\/status\/[0-9]+/i;
 
-// only do the embedding for a single twitter url on its own paragraph.
+const getEmbeddedTweet = async (url, opt) => {
+  const embedOptions = {
+    url: url,
+    hide_thread: opt.hideThread !== false ? '1' : '0',
+    align: opt.align || '',
+    hide_media: opt.hideMedia ? '1' : '0',
+    theme: opt.theme || '',
+    link_color: opt.linkColor || '',
+    widget_type: opt.widgetType || '',
+    omit_script: true,
+    dnt: true,
+    limit: 20,
+    chrome: 'nofooter'
+  }
+  const params = Object.entries(embedOptions).map(([key, val]) => `${key}=${val}`).join('&')
+  const apiUrl = `https://publish.twitter.com/oembed?${params}`
+  const response = await fetch(apiUrl)
+  return await response.json()
+}
+
 const isTwitterLink = node => {
-  return node.children.length === 1 && node.children[0].type === 'link' &&
-    (tweetRegexp.test(node.children[0].url) || momentRegexp.test(node.children[0].url))
+  return node.children.length === 1 && node.children[0].type === 'link' 
+  && (tweetRegexp.test(node.children[0].url) || momentRegexp.test(node.children[0].url))
 }
 
 module.exports = (options) => {
